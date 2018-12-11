e3bc85db1d2cbcb1e992efc2dd812235a0a5deba
Choco <trust2065@gmail.com>
2018-12-11 21:37:57 +1100

    promise all

--- Promise.js
+++ Promise.js
@@ -1,3 +1,10 @@
+// Promise is an javascript Object
+// use 'async' before function declaration, the function will return a promise object
+// user 'await' before calling function, the function will get process, just like then()
+// we can you try catch to handle all exceptions
+// use Promise all, not garuntee order, but garuntee all processed.
+// give error when 'any' of the promise get rejected
+
 async function getUsers() {
     return ([{
             name: "Choco",
@@ -18,6 +25,8 @@ function getFirstUser() {
 }
 
 async function main() {
+    console.log('\n--------Promise--------\n');
+
     console.log(getFirstUser().then((val) => {
         console.log(val);
     }));
@@ -26,6 +35,24 @@ async function main() {
 
     const firstUser2 = getFirstUser();
     console.log(`firstUser2: ${firstUser2}`);
+
+    
+    console.log('\n--------Promise all--------');
+    const arr = [{
+        key: 1
+    }, {
+        key: 2
+    }, {
+        key: 3
+    }]
+
+    const results = arr.map(async (obj) => {
+        return obj.key;
+    });
+    // document.writeln( `Before waiting: ${results}`);
+
+    Promise.all(results).then((completed) => console.log(`\nResult: ${completed}`));
+    console.log(`\nResult without Promise all: ${results}`)
 }
 
 main();
